b86780df41427786452b19cf0a5dd02d7dc8232e
Pofay <gianCarlo.gilos@gmail.com>
2025-02-03 19:23:32 +0800

    tutorial: completed section 92

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,22 +4,25 @@ defmodule Discuss.TopicController do
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
-    render conn, "index.html", topics: topics
+    render(conn, "index.html", topics: topics)
   end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render conn, "new.html", changeset: changeset
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
+
     case Repo.insert(changeset) do
-      {:ok, topic} ->
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Created")
         |> redirect(to: topic_path(conn, :index))
-      {:error, changeset} -> render conn, "new.html", changeset: changeset
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
     end
   end
 
@@ -27,6 +30,21 @@ defmodule Discuss.TopicController do
     topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(topic)
 
-    render conn, "edit.html", changeset: changeset, topic: topic
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, topic: old_topic)
+    end
   end
 end
